9dbac198d18eeb2fc91b214e91fdb3dc50f9b0da
shindyu <shindyu@shindyu-no-MacBook-Pro.local>
2017-01-31 00:07:42 +0900

    Adds sound

--- game/TestScene.swift
+++ game/TestScene.swift
@@ -3,10 +3,11 @@ import GameplayKit
 
 class TestScene: SKScene, SKPhysicsContactDelegate {
     private var label: SKLabelNode?
-    var paddle: SKSpriteNode!
+    var paddle: SKShapeNode!
     var left: CGFloat?
     var right: CGFloat?
     var blocks = [SKShapeNode]()
+    var moving: Bool = false
 
     override func didMove(to view: SKView) {
         self.physicsBody = SKPhysicsBody(edgeLoopFrom: self.frame)
@@ -14,87 +15,109 @@ class TestScene: SKScene, SKPhysicsContactDelegate {
         left = -(self.frame.width/2.0)
         right = (self.frame.width/2.0)
 
-        initSceneBalls()
+        //initSceneBalls()
         //initSceneBlocks()
+        initPaddle()
+
+        Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(TestScene.dropBall), userInfo: nil, repeats: true)
     }
 
-    func initSceneBlocks() {
-        for i in 0..<5 {
-            let block = SKShapeNode(rectOf: CGSize(width: 100.0, height: 50.0))
-
-            block.position = CGPoint(x: left! + 50.0 + CGFloat(110.0 * Double(i)), y: 10.0)
-            block.fillColor = .gray
-            block.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width:100, height:50))
-            block.physicsBody?.contactTestBitMask = 1
-            block.physicsBody?.affectedByGravity = false
-            block.physicsBody?.isDynamic = false
-            block.name = "block\(i)"
-
-            self.addChild(block)
-
-            self.blocks.append(block)
-        }
-
-    }
-
-    func initSceneBalls() {
-        //self.physicsBody = SKPhysicsBody(edgeLoopFrom: self.frame)
-
-        //self.physicsWorld.gravity = CGVectorMake(0, self.physicsWorld.gravity.dy * -1.0)
-        for i in 0 ..< 30 {
-            let ball = newBall()
-            let x: CGFloat = CGFloat(CGFloat(i) * self.frame.width / 10.0)
-            let y = i % 10
-            ball.position = CGPoint(x: x, y: 30 * CGFloat(y))
-            ball.physicsBody?.affectedByGravity = false
-            ball.physicsBody?.contactTestBitMask = 1
-            self.addChild(ball)
-        }
+    func initPaddle() {
+        paddle = SKShapeNode(circleOfRadius: 100)
+        paddle.position = CGPoint(x: self.frame.midX, y: self.frame.midY)
+        paddle.physicsBody = SKPhysicsBody(circleOfRadius: 100)
+        paddle.physicsBody?.contactTestBitMask = 1
+        paddle.physicsBody?.isDynamic = false
+        paddle.fillColor = .brown
+        self.addChild(paddle)
     }
 
     func newBall() -> SKNode {
-        let colors:[SKColor] = [.blue, .red, .green, .yellow, .purple]
-        let index = (Int)(arc4random_uniform(4))
-        let color = colors[index]
-        // このradiusの値とphysicsbodyの値の設定は同じにしとくっポイ
+        let colors:[SKColor] = [.red, .blue, .green, .yellow, .purple]
+        let index = (Int)(arc4random_uniform(5))
+        let color: SKColor = colors[index]
         let ball = SKShapeNode(circleOfRadius: 30.0)
         ball.strokeColor = SKColor.black
         ball.fillColor = color
+        ball.lineWidth = 0
         ball.physicsBody = SKPhysicsBody(circleOfRadius: 30.0)
+        ball.physicsBody?.contactTestBitMask = 1
+        ball.physicsBody?.restitution = 1.0
+        ball.physicsBody?.linearDamping = 0
+        ball.physicsBody?.friction = 0
+        switch color {
+        case SKColor.red:
+            ball.name = "ball_red"
+        case SKColor.blue:
+            ball.name = "ball_blue"
+        case SKColor.green:
+            ball.name = "ball_green"
+        case SKColor.yellow:
+            ball.name = "ball_yellow"
+        case SKColor.purple:
+            ball.name = "ball_purple"
+        default:
+            ball.name = "ball"
+        }
 
         return ball
     }
 
-
-    func skRand(low: CGFloat, high: CGFloat) -> CGFloat {
-        return CGFloat(Float(arc4random()) / Float(UINT32_MAX)) * (high - low) + low;
+    func dropBall() {
+        if moving {
+            let ball = newBall()
+            ball.position = CGPoint(x: paddle.frame.midX, y: self.frame.maxY - 20.0)
+            self.addChild(ball)
+        }
     }
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        moving = true
+    }
+
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         let touch: UITouch = touches.first!
         let location = touch.location(in: self)
+        paddle.position = CGPoint(x: location.x, y: location.y)
+    }
 
-        let colors:[SKColor] = [.blue, .red, .green, .yellow, .purple]
-        let index = (Int)(arc4random_uniform(4))
-        let color = colors[index]
-        // このradiusの値とphysicsbodyの値の設定は同じにしとくっポイ
-        let circle = SKShapeNode(circleOfRadius: 30.0)
-        circle.position = CGPoint(x:location.x, y:location.y)
-        circle.strokeColor = SKColor.black
-        circle.fillColor = color
-        circle.physicsBody = SKPhysicsBody(circleOfRadius: 30.0)
-
-
-        self.addChild(circle)
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        moving = false
     }
 
     func didBegin(_ contact: SKPhysicsContact) {
-        if let block = contact.bodyA.node as? SKShapeNode {
-            if blocks.contains(block) {
-                block.removeFromParent()
-            }
+        guard let nodeA = contact.bodyA.node as? SKShapeNode,
+            let nodeB = contact.bodyB.node as? SKShapeNode else {
+            return
         }
-        contact.bodyA.node?.physicsBody?.affectedByGravity = true
-        contact.bodyB.node?.physicsBody?.affectedByGravity = true
+        if nodeA.name?.contains("ball") == true {
+            nodeA.physicsBody?.affectedByGravity = false
+        }
+        if nodeB.name?.contains("ball") == true {
+            nodeB.physicsBody?.affectedByGravity = false
+        }
+
+        if nodeA.name == nodeB.name {
+            var soundFile = "piano_do.mp3"
+            if nodeA.name == "ball_red" {
+                soundFile = "piano_do.mp3"
+            }
+            if nodeA.name == "ball_blue" {
+                soundFile = "piano_re.mp3"
+            }
+            if nodeA.name == "ball_green" {
+                soundFile = "piano_mi.mp3"
+            }
+            if nodeA.name == "ball_yellow" {
+                soundFile = "piano_fa.mp3"
+            }
+            if nodeA.name == "ball_purple" {
+                soundFile = "piano_so.mp3"
+            }
+            let action = SKAction.playSoundFileNamed(soundFile, waitForCompletion: true)
+            self.run(action)
+            nodeA.removeFromParent()
+            nodeB.removeFromParent()
+        }
     }
 }
